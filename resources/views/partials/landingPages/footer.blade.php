9a2f2cf0d49989d2c9a3564c4190c0f21db9f7f4
Farid Imam Nur S <114373977+Faridimamns@users.noreply.github.com>
2023-12-25 23:08:33 +0700

    edit landing pages

--- resources/views/partials/landingPages/footer.blade.php
+++ resources/views/partials/landingPages/footer.blade.php
@@ -1,58 +1,10 @@
 <footer class="footer-area section-gap">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-2  col-md-6">
-                <div class="single-footer-widget">
-                    <h6>Top Products</h6>
-                    <ul class="footer-nav">
-                        <li><a href="#">Managed Website</a></li>
-                        <li><a href="#">Manage Reputation</a></li>
-                        <li><a href="#">Power Tools</a></li>
-                        <li><a href="#">Marketing Service</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4  col-md-6">
-                <div class="single-footer-widget mail-chimp">
-                    <h6 class="mb-20">Contact Us</h6>
-                    <p>
-                        56/8, bir uttam qazi nuruzzaman road, west panthapath, kalabagan, Dhanmondi, Dhaka - 1205
-                    </p>
-                    <h3>012-6532-568-9746</h3>
-                    <h3>012-6532-568-97468</h3>
-                </div>
-            </div>							
-            <div class="col-lg-6  col-md-12">
-                <div class="single-footer-widget newsletter">
-                    <h6>Newsletter</h6>
-                    <p>You can trust us. we only send promo offers, not a single spam.</p>
-                    <div id="mc_embed_signup">
-                        <form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
-
-                            <div class="form-group row" style="width: 100%">
-                                <div class="col-lg-8 col-md-12">
-                                    <input name="EMAIL" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address '" required="" type="email">
-                                    <div style="position: absolute; left: -5000px;">
-                                        <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-                                    </div>
-                                </div> 
-                            
-                                <div class="col-lg-4 col-md-12">
-                                    <button class="nw-btn primary-btn">Subscribe<span class="lnr lnr-arrow-right"></span></button>
-                                </div> 
-                            </div>		
-                            <div class="info"></div>
-                        </form>
-                    </div>		
-                </div>
-            </div>					
-        </div>
-
         <div class="row footer-bottom d-flex justify-content-between">
-            <p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            <p class="col-lg-5 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> Kelompok Pisang - Pagi <i class="" aria-hidden="true"></i> by <a href="https://sobatsehat.com" target="_blank">RPL 22TI</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-            <div class="col-lg-4 col-sm-12 footer-social">
+            <div class="col-lg-2 col-sm-12 footer-social">
                 <a href="#"><i class="fa fa-facebook"></i></a>
                 <a href="#"><i class="fa fa-instagram"></i></a>
             </div>
