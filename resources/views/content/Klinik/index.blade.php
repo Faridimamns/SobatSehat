1f62f3335e3f6e5f5701bd476d7357b2f5885f17
Farid Imam Nur S <114373977+Faridimamns@users.noreply.github.com>
2023-12-25 14:55:39 +0700

    layouting dashboard

---
+++ resources/views/content/Klinik/index.blade.php
@@ -0,0 +1,4 @@
+@extends('partials.dashboard.main')
+@section('content')
+    <h1>ini adalah rekomen klinik</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/navbar', function () {
+    return view('partials.dashboard.navbar');
+});
+Route::get('/side', function () {
+    return view('partials.dashboard.sidebar');
+});
+Route::get('/main', function () {
+    return view('partials.dashboard.main');
+});
+
+Route::get('/klinik', function () {
+    return view('content.klinik.index');
+});
