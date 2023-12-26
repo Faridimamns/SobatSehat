8bedca678f34b6cb7b8ddcf54f33e9aa19c80982
sh1z1a <h4sn44ziz4h@gmail.com>
2023-12-26 20:30:57 +0700

    Update route web

--- resources/views/content/Klinik/index.blade.php
+++ resources/views/content/Klinik/index.blade.php
@@ -26,45 +26,6 @@
                         <td>Jl. Margonda No.100</td>
                         <td>Table cell</td>
                         <td>
-                            <a href="/klinik"><i class="la la-edit la-lg"></i></a>
-                            <a href="/klinik"><i class="la la-trash la-lg"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Klinik Permata Indah 2</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <a href="/klinik"><i class="la la-edit la-lg"></i></a>
-                            <a href="/klinik"><i class="la la-trash la-lg"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Klinik Mas Medis</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <a href="/klinik"><i class="la la-edit la-lg"></i></a>
-                            <a href="/klinik"><i class="la la-trash la-lg"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Klinik Widyaguna Health</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>  
-                            <a href="/klinik"><i class="la la-edit la-lg"></i></a>
-                            <a href="/klinik"><i class="la la-trash la-lg"></i></a></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Klinik Kinan</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
                             <a href="/klinik" style="color: orange; font-size: 20px; font-weight: bold;"><i class="la la-edit la-lg"></i></a>
                             <a href="/klinik" style="color: red; font-size: 20px; font-weight: bold;"><i class="la la-trash la-lg"></i></a>
                         </td>
--- routes/web.php
+++ routes/web.php
@@ -14,50 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('content.LandingPages.index');
-});
-
-Route::get('/berita', function () {
-    return view('content.LandingPages.berita');
-});
 
 
-Route::get('/navbar', function () {
-    return view('partials.dashboard.navbar');
-});
-Route::get('/side', function () {
-    return view('partials.dashboard.sidebar');
-});
-Route::get('/main', function () {
-    return view('partials.dashboard.main');
-});
 
-// Route::get('/klinik', function () {
-//     return view('content.Klinik');
-// });
-
-Route::get('/home', function () {
-    return view('partials.landingPages.main');
-});
-Route::get('/home/nav', function () {
-    return view('partials.landingPages.navbar');
-});
-Route::get('/home/foot', function () {
-    return view('partials.landingPages.footer');
-});
-
-
-Route::get('/klinik', [KlinikController::class, 'index']);
-Route::get('/klinikcreate', [KlinikController::class, 'create']);
-Route::get('/klinikedit', [KlinikController::class, 'edit']);
-
-// Route::get('/klinik', function () {
-//     return view('content.klinik.index');
-// });
-// Route::get('/klinikedit', function () {
-//     return view('content.klinik.edit');
-// });
-// Route::get('/kliniktambah', function () {
-//     return view('content.klinik.tambah');
-// });
