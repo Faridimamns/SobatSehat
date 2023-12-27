663c7887f307fc9fa2ee7cab6b9de3fe45df9b2b
Farid Imam Nur S <114373977+Faridimamns@users.noreply.github.com>
2023-12-27 11:46:22 +0700

    migration and web and models

--- app/Models/Jadwal.php
+++ app/Models/Jadwal.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Jadwal extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'lokasi',
+        'alamat',
+        'link',
+        'tanggal',
+        'gambar',
+    ];
 }
--- resources/views/partials/dashboard/navbar.blade.php
+++ resources/views/partials/dashboard/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="main-header">
     <div class="logo-header">
         <a href="">SOBAT SEHAT</a>
-        <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse"
-            data-target="collapse" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse"
+            aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <button class="topbar-toggler more"><i class="la la-ellipsis-v"></i></button>
@@ -21,100 +21,20 @@
                 </div>
             </form>
             <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
-                <li class="nav-item dropdown hidden-caret">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="la la-envelope"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown hidden-caret">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="la la-bell"></i>
-                        <span class="notification">3</span>
-                    </a>
-                    <ul class="dropdown-menu notif-box" aria-labelledby="navbarDropdown">
-                        <li>
-                            <div class="dropdown-title">You have 4 new notification</div>
-                        </li>
-                        <li>
-                            <div class="notif-center">
-                                <a href="#">
-                                    <div class="notif-icon notif-primary"> <i class="la la-user-plus"></i>
-                                    </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            New user registered
-                                        </span>
-                                        <span class="time">5 minutes ago</span>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div class="notif-icon notif-success"> <i class="la la-comment"></i> </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            Rahmad commented on Admin
-                                        </span>
-                                        <span class="time">12 minutes ago</span>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div class="notif-img">
-                                        <img src="/assetsDashboard/img/profile2.jpg" alt="Img Profile">
-                                    </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            Reza send messages to you
-                                        </span>
-                                        <span class="time">12 minutes ago</span>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div class="notif-icon notif-danger"> <i class="la la-heart"></i> </div>
-                                    <div class="notif-content">
-                                        <span class="block">
-                                            Farrah liked Admin
-                                        </span>
-                                        <span class="time">17 minutes ago</span>
-                                    </div>
-                                </a>
-                            </div>
-                        </li>
-                        <li>
-                            <a class="see-all" href="javascript:void(0);"> <strong>See all
-                                    notifications</strong> <i class="la la-angle-right"></i> </a>
-                        </li>
-                    </ul>
-                </li>
                 <li class="nav-item dropdown">
-                    <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"
-                        aria-expanded="false"> <img src="/assetsDashboard/img/profile.jpg" alt="user-img"
-                            width="36" class="img-circle"><span>Hizrian</span></span> </a>
+                    <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
+                        {{-- <img src="/assetsDashboard/img/profile.jpg" alt="user-img" width="36" class="img-circle"> --}}
+                        <svg width="15" height="15" viewBox="0 0 50 58" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M25 28.5714C32.8906 28.5714 39.2857 22.1763 39.2857 14.2857C39.2857 6.39509 32.8906 0 25 0C17.1094 0 10.7143 6.39509 10.7143 14.2857C10.7143 22.1763 17.1094 28.5714 25 28.5714ZM35 32.1429H33.1362C30.6585 33.2812 27.9018 33.9286 25 33.9286C22.0982 33.9286 19.3527 33.2812 16.8638 32.1429H15C6.71875 32.1429 0 38.8616 0 47.1429V51.7857C0 54.7433 2.39955 57.1429 5.35714 57.1429H44.6429C47.6004 57.1429 50 54.7433 50 51.7857V47.1429C50 38.8616 43.2812 32.1429 35 32.1429Z"
+                                fill="black" />
+                        </svg>
+
+                        <span>Hizrian</span></a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li>
-                            <div class="user-box">
-                                <div class="u-img"><img src="/assetsDashboard/img/profile.jpg"
-                                        alt="user"></div>
-                                <div class="u-text">
-                                    <h4>Hizrian</h4>
-                                    <p class="text-muted">hello@themekita.com</p><a href="profile.html"
-                                        class="btn btn-rounded btn-danger btn-sm">View Profile</a>
-                                </div>
-                            </div>
-                        </li>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#"><i class="ti-user"></i> My Profile</a>
-                        <a class="dropdown-item" href="#"></i> My Balance</a>
-                        <a class="dropdown-item" href="#"><i class="ti-email"></i> Inbox</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#"><i class="ti-settings"></i> Account
-                            Setting</a>
+
+                        <a class="dropdown-item" href="{{ url('/')}}"><i class="ti-settings"></i> Back To Home</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#"><i class="fa fa-power-off"></i> Logout</a>
                     </ul>
