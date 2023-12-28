c08b2f0acd149dc1cb61944aebf52895da2d5502
DadinAJ <dadinaj4@gmail.com>
2023-12-28 09:22:59 +0700

    perbikan halaman dashboard jadwal

--- resources/views/content/JadwalAdmin/create.blade.php
+++ resources/views/content/JadwalAdmin/create.blade.php
@@ -8,43 +8,23 @@
     </div>
     
     <div class="card-body">
-
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-        <form>
-        <div class="form-group row">
-            <label for="lokasi" class="col-3 col-form-label">Lokasi Klinik</label> 
-            <div class="col-9">
-            <input id="lokasi" name="lokasi" type="text" class="form-control">
-            </div>
+        <div class="form-group">
+            <label for="text">Lokasi Klinik</label>
+            <input type="text" class="form-control" id="text" placeholder="Masukkan Judul">
         </div>
-        <div class="form-group row">
-            <label for="alamat" class="col-3 col-form-label">Alamat</label> 
-            <div class="col-9">
-            <textarea id="alamat" name="alamat" cols="40" rows="4" class="form-control"></textarea>
-            </div>
+        <div class="form-group">
+            <label for="text-area">Alamat Klinik</label>
+            <input type="text-area" class="form-control" id="text-area" placeholder="Masukkan Informasi">
         </div>
-        <div class="form-group row">
-            <label for="link" class="col-3 col-form-label">Link Form</label> 
-            <div class="col-9">
-            <input id="link" name="link" type="text" class="form-control">
-            </div>
-        </div> 
-        <div class="form-group row">
-            <label for="tanggal" class="col-3 col-form-label">Tanggal Konsultasi:</label>
-            <div class="col-5">
-            <input type="date" id="tanggal" name="tanggal" class="form-control" required>
-            </div>
+        <div class="form-group">
+            <label for="link">Link</label>
+            <input type="link" class="form-control" id="link" placeholder="Masukkan Link">
         </div>
-
-        <div class="form-group row">
-            <div class="col-9">
-            <button name="cancel" type="cancel" class="btn btn-danger">Cancel</button>
-            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-            </div>
+        <div class="mt-2 mb-2 p-2">
+            <button type="button" class="btn btn-danger">Cancel</button>
+            <button type="button" class="btn btn-info">Submit</button>
         </div>
-        </form>
+        
 
         {{-- <form action="/proses-form" method="post">
             <label for="tanggal">Tanggal:</label>
